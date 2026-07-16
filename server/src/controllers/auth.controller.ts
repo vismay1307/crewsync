2dde81a86fe9ab7d812f0d3b4cbbea53c02bc6b0
vismaythakkar-admin <sepl.vismaythakkar@gmail.com>
2026-07-16 15:38:47 +0530

    feat: add zod validation library and refactor signup response

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,16 +1,19 @@
-import {Request , Response} from "express";
+import { Request, Response } from "express";
+
 import ApiResponse from "../utils/ApiResponse.js";
+import { signupService } from "../services/auth.services.js";
 
-export const signup = async(
-    req:Request,
-    res:Response
+export const signup = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const user = await signupService(req.body);
 
-):Promise<void>=>{
-     res.status(201).json(
-        new ApiResponse(
-            201,
-            "Sign Up Is Working",
-            req.body
-        )
-    );
-}
+  res.status(201).json(
+    new ApiResponse(
+      201,
+      "User registered successfully",
+      user
+    )
+  );
+};
